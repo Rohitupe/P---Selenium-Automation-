86560570ae808a486dee046a6c64e63d688143ca
Rohitupe <rohittupe21@gmail.com>
2021-01-16 11:15:26 +0530

    Update

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
-from selenium import webdriver
-import time
+# from selenium import webdriver
+# import time
+#
+# driver = webdriver.Edge(executable_path="driver/msedgedriver.exe")
+#
+# driver.get("https://www.google.com")
+# time.sleep(10)
+# driver.quit()
 
-driver = webdriver.Edge(executable_path="driver/msedgedriver.exe")
+# list1 = [1,2,3,4]
+# list2 = [4,6,21,1]
+list1 = input().split(' ')
 
-driver.get("https://www.google.com")
-time.sleep(10)
-driver.quit()
+list2 = input().split(' ')
+mainlist = list1+list2
+
+mainlist.sort()
+print(mainlist)
