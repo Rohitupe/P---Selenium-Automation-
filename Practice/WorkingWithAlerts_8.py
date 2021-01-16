bb0603064b3b7bc0fb15edb004c7d698e8f916ca
Rohitupe <rohittupe21@gmail.com>
2021-01-16 11:57:56 +0530

    Workin with alert window popup message

---
+++ Practice/WorkingWithAlerts_8.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Edge(executable_path="../Driver/msedgedriver.exe")
+driver.implicitly_wait(10)
+driver.maximize_window()
+
+"""
+    - switch_to_alert().accept()
+    - switch_to_alert().dismiss()
+"""
+
+driver.get("http://testautomationpractice.blogspot.com/")
+
+#  click on click me button
+driver.find_element(By.XPATH,'//*[@id="HTML9"]/div[1]/button').click()
+
+time.sleep(5)
+
+# driver.switch_to_alert().accept() # click on ok button on alert window message
+
+driver.switch_to.alert.dismiss() # click on cancel button on alert window message
+
+driver.close()
+driver.quit()
