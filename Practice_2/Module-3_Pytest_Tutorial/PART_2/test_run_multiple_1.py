064267199e95e829e15c93eeabd903f565a371f5
Rohitupe <rohittupe21@gmail.com>
2021-05-01 21:50:56 +0530

    running selected test files

---
+++ Practice_2/Module-3_Pytest_Tutorial/PART_2/test_run_multiple_1.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def test_run_one():
+    print("Run Single File")
+
+
+def test_runsingletest():
+    print("Run Single test case")
+
+
+def test_cred_part2():
+    print("test run multiple 1 --> part 2")
